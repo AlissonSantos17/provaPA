51f1d9191099e9fbfe93911b721772f46cd356f3
Alisson Felipe Lima Santos <Alisson.Santos@cencosud.com.br>
2020-12-07 17:03:49 -0300

    Swagger

--- src/routes/funcionarios.routes.js
+++ src/routes/funcionarios.routes.js
@@ -3,6 +3,6 @@ const FuncionarioController = require('../controllers/Funcionario.controller');
 
 funcionarioController = Router();
 
-funcionarioController.get('/maiorsalario', FuncionarioController.getMaiorSalario);
+funcionarioController.post('/maiorsalario', FuncionarioController.getMaiorSalario);
 
 module.exports = funcionarioController;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 
 const port = process.env.PORT | 3000;
@@ -9,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors());
 app.use(routes)
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
   console.log(`Server running in localhost:${port}!`)
